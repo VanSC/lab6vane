409b737460f7b5530df49ba487bfe88d5a06f5f7
VanSC <vanessa.choque@tecsup.edu.pe>
2024-04-26 19:37:36 -0500

    Agregar archivos de proyecto.

---
+++ Lab6vane/Menu.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab6vane
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        string connectionString = "Data Source=VANESSA\\SQLEXPRESS;Initial Catalog=NeptunoDB;User Id=vanne;Password=123456";
+        public Menu()
+        {
+            InitializeComponent();
+            showproducts();
+        }
+
+        public void showproducts()
+        {
+            try
+            {
+                List<Producto> productos = new List<Producto>();
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("sp_GetProdcuts", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                // CONECTADA
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string nombreProducto = reader.GetString("nombreProducto");
+                    string cantidadPorUnidad = reader.GetString("cantidadPorUnidad");
+                    decimal precioUnidad = reader.GetDecimal("precioUnidad");
+                    // string categoriaProducto = reader.GetString("categoriaProducto");
+                    string categoriaProducto = reader.IsDBNull(reader.GetOrdinal("categoriaProducto")) ? null : reader.GetString("categoriaProducto");
+
+                    productos.Add(new Producto
+                    {
+                        NombreProducto = nombreProducto,
+                        CantidadPorUnidad = cantidadPorUnidad,
+                        PrecioUnidad = precioUnidad,
+                        CategoriaProducto = categoriaProducto
+                    });
+
+                }
+
+                // Crear una nueva lista con solo las columnas deseadas
+                var listaFiltrada = productos.Select(p => new
+                {
+                    Producto = p.NombreProducto,
+                    cantidadPorUnidad = p.CantidadPorUnidad,
+                    precioUnidad = p.PrecioUnidad,
+                    categoriaProducto = p.CategoriaProducto
+                }).ToList();
+
+                listproductos.ItemsSource = listaFiltrada;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los productos: {ex.Message}");
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrarProducto registrarform = new RegistrarProducto();
+            registrarform.ShowDialog();
+        }
+    }
+}
---
+++ Lab6vane/RegistrarProducto.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab6vane
+{
+    /// <summary>
+    /// Lógica de interacción para RegistrarProducto.xaml
+    /// </summary>
+    public partial class RegistrarProducto : Window
+    {
+        string connectionString = "Data Source=VANESSA\\SQLEXPRESS;Initial Catalog=NeptunoDB;User Id=vanne;Password=123456";
+
+        public RegistrarProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrarProcutoClick(object sender, RoutedEventArgs e)
+        {
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("sp_AddProduct", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    //Valores
+                    cmd.Parameters.AddWithValue("@nombreProducto", txtProductName.Text);
+                    cmd.Parameters.AddWithValue("@idProveedor", int.Parse( txtProveedor.Text));
+                    cmd.Parameters.AddWithValue("@idCategoria", int.Parse(txtCategoria.Text));
+                    cmd.Parameters.AddWithValue("@cantidadPorUnidad", txtCantUni.Text);
+                    cmd.Parameters.AddWithValue("@precioUnidad", decimal.Parse(txtPrecioUnidad.Text));
+                    cmd.Parameters.AddWithValue("@unidadesEnExistencia", int.Parse(txtUniEnExistencia.Text));
+                    cmd.Parameters.AddWithValue("@unidadesEnPedido", int.Parse(txtUniPedido.Text));
+                    cmd.Parameters.AddWithValue("@nivelNuevoPedido", int.Parse(txtNivelPedido.Text));
+                    cmd.Parameters.AddWithValue("@suspendido", int.Parse(txtSuspedido.Text));
+                    cmd.Parameters.AddWithValue("@categoriaProducto", txtCategoriaProducto.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto registrado correctamente :D");
+                }
+            }
+        }
+
+        private void ButtonCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Menu list = new Menu();
+            list.ShowDialog();
+        }
+    }
+}
